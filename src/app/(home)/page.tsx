2421e8a0503ce27a14a002bead3f4db4fa01cbf2
Bruno Castro <bruno.castro@sysmap.com.br>
2023-10-18 22:33:19 -0300

    feat: add Categories and CategoryItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
