f217adf53ddc06ac09eed98ac24c2e671a7f274e
Bruno Castro <bruno.castro@sysmap.com.br>
2023-10-20 14:48:39 -0300

    fix: ensure product prices are wrapper in one line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -9,8 +9,8 @@ type ProductItemProps = {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+    <div className="flex max-w-[170px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           alt={product.name}
