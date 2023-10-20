08243aea4a5b68cf9ff2546fc245ffb7728752b2
Bruno Castro <bruno.castro@sysmap.com.br>
2023-10-20 14:45:09 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      2023 Copyright <span className="font-semibold">Store Next</span>
+    </div>
+  );
+};
